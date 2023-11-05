103c0310a95b1cb014b9d2b34a8b8fc605341c17
ParnianFattahi <148445422+ParnianFattahi@users.noreply.github.com>
2023-11-05 00:14:17 -0700

    Add files via upload

---
+++ HM5-1.py
@@ -0,0 +1,7 @@
+n=int(input('enter the number of rows: '))
+m=int(input(' enter the number of colmun: '))
+for i in range(n):
+    for j in range(m):
+        print ('#*',end='')
+
+        print ()
